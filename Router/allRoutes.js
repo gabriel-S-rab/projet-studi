6639282d8b5a89c3310aa66ea57259bd256df38e
gabriel-S-rab <serguei.roos@laposte.net>
2025-05-26 22:31:07 +0200

    ajout elemnt auth

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie","La Galerie","/pages/galerie.html"),
+    new Route("/signin","Connexions","/pages/auth/signin.html"),
+    new Route("/signup","Inscrption","/pages/auth/signup.html"),
+    new Route("/account","Mon compte","/pages/auth/account.html"),
+    new Route("/editPassword","changement mot de passe","/pages/auth/editPassword.html"),
 ];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
